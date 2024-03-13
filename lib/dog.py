62a3f535c90993d91d70e929a9ba6358ed7d3f3c
victormuteithia <muteithiavictor@gmail.com>
2024-03-13 16:31:15 +0300

    Completed assignment

--- lib/dog.py
+++ lib/dog.py
@@ -1,7 +1,15 @@
 #!/usr/bin/env python3
 
 class Dog:
-    # Class body goes here
 
-    #Instance method definition
-    pass
+    def bark(self):
+        print("Woof!")
+
+    def sit(self):
+        print("The dog is sitting.")
+
+fido = Dog()
+fido.bark()
+
+snoppy = Dog()
+snoppy.bark()
